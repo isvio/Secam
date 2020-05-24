038ab8186595b4f72c0c99922cf2e9149b77b6f2
Viorel Stefan <65833391+ivios@users.noreply.github.com>
2020-05-24 11:05:22 +0200

    old files from sololearn

---
+++ app.js
@@ -0,0 +1,69 @@
+jQuery(function () {
+    $("#playButton").on("click", function () {
+        $("#begin").hide();
+        $("#gamePlace").show();
+        $("#c11,#c21,#c22").css("background-color", "#4481ba");
+        $("#c12,#c13,#c23").css("background-color", "#92b789");
+        $("#c31,#c32").css("background-color", "#602b24");
+        $("#c33").css("background-color", "#b7758f");
+    });
+
+    $("#helpButton").on("click", function () {
+        $("#begin").hide();
+        $("#help").show();
+        $("#helpBack").on("click", function () {
+            $("#help").hide();
+            $("#begin").show();
+        })
+    })
+});
+
+function condition(x, y, z) {
+    return !((x === y) && (y === z) && (x === z));
+}
+
+function game() {
+    var
+        num1 = document.getElementById("c11").value,
+        num2 = document.getElementById("c12").value,
+        num3 = document.getElementById("c13").value,
+        num4 = document.getElementById("c21").value,
+        num6 = document.getElementById("c23").value,
+        num7 = document.getElementById("c31").value,
+        num8 = document.getElementById("c32").value,
+        num9 = document.getElementById("c33").value;
+    if (isNaN(parseInt(num1)))
+        document.getElementById("c11").style.border = "2px solid red";
+    if (isNaN(parseInt(num2)))
+        document.getElementById("c12").style.border = "2px solid red";
+    if (isNaN(parseInt(num3)))
+        document.getElementById("c13").style.border = "2px solid red";
+    if (isNaN(parseInt(num4)))
+        document.getElementById("c21").style.border = "2px solid red";
+    if (isNaN(parseInt(num6)))
+        document.getElementById("c23").style.border = "2px solid red";
+    if (isNaN(parseInt(num7)))
+        document.getElementById("c31").style.border = "2px solid red";
+    if (isNaN(parseInt(num8)))
+        document.getElementById("c32").style.border = "2px solid red";
+    if (isNaN(parseInt(num9)))
+        document.getElementById("c33").style.border = "2px solid red";
+    if (condition(parseInt(num1), parseInt(num2), parseInt(num3)) &&
+        condition(parseInt(num4), 2, parseInt(num6)) &&
+        condition(parseInt(num7), parseInt(num8), parseInt(num9)) &&
+        condition(parseInt(num1), parseInt(num4), parseInt(num7)) &&
+        condition(parseInt(num2), 2, parseInt(num8)) &&
+        condition(parseInt(num4), parseInt(num6), parseInt(num9))) {
+        if (parseInt(num1) + parseInt(num4) === 11 && parseInt(num7) + parseInt(num8) === 5 &&
+            parseInt(num2) + parseInt(num3) + parseInt(num6) === 8 && parseInt(num9) === 6) {
+            document.getElementById("verify").style.display = "none";
+            document.getElementById("win").style.display = "block";
+        } else {
+            document.getElementById("verify").style.display = "none";
+            document.getElementById("lose").style.display = "block";
+        }
+    } else {
+        document.getElementById("verify").style.display = "none";
+        document.getElementById("lose").style.display = "block";
+    }
+}
